17360fdfe73419338c5bb148532ae926acad6972
Jumeil John Navares <jumeil.navares@gmail.com>
2025-07-27 01:28:09 +0800

    Work in progress: Using tailwind.

---
+++ vite-vue-tailwind/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="text-blue-500 font-bold text-2xl">Hello Tailwind</h1>
+</template>
+
+<script setup>
+</script>
---
+++ vite-vue-tailwind/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://buildwithhussain.com',
+        changeOrigin: true,
+        secure: true,
+        followRedirects: true
+      }
+    }
+  }
+})
