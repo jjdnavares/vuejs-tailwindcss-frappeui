5982a9d42b1d7d885677d354882abc44fcdf32ee
Jumeil John Navares <jumeil.navares@gmail.com>
2025-07-27 02:03:01 +0800

    Tailwind + Vue slots

--- vite-vue-tailwind/src/App.vue
+++ vite-vue-tailwind/src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <h1 class="text-blue-500 font-bold text-2xl">Hello Tailwind</h1>
+  <div class="m-5">
+    <!-- <Badge status="Inactive" />
+    <button class="bg-slate-800 text-white px-2 py-1 rounded-sm">Click</button> -->
+
+    <Badge>Active</Badge>
+    <!-- <Badge>Inactive</Badge>
+    <Badge>Paid</Badge>
+    <Badge>Unpaid</Badge>
+    <Badge>Pending</Badge> -->
+  </div>
 </template>
 
 <script setup>
+import Badge from './Badge.vue';
+
 </script>
---
+++ vite-vue-tailwind/src/Badge.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="[
+        colorToClasses[color],
+        'flex w-fit px-3 py-1 text-s font-semibold rounded-2xl'
+    ]">
+        <!-- {{ props.status }} -->
+        <slot></slot>
+    </div>
+    <!-- <div> {{ color }} </div> -->
+</template>
+
+<script setup>
+import { computed, useSlots, watch} from "vue";
+
+const slots = useSlots();
+// const children = slots.default()[0].children;
+// console.log(children);
+
+const statusToColor = {
+    Active: "green",
+    Inactive: "red",
+    Paid: "blue",
+    Unpaid: "orange",
+    Pending: "yellow",
+}
+
+const colorToClasses = {
+    green: "text-green-700 bg-green-300",
+    red: "text-red-700 bg-red-300",
+    blue: "text-blue-700 bg-blue-300",
+    orange: "text-orange-700 bg-orange-300",
+    yellow: "text-yellow-700 bg-yellow-300",
+}
+
+const props = defineProps({
+    status: String,
+})
+
+const color = computed(() => {
+    let children = slots.default()[0].children.trim();
+    return statusToColor[children]
+})
+
+watch(color, (value) =>{
+    console.log(value);
+})
+</script>
