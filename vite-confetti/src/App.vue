16f128994fd406617e7ef4934f58271a2d3d9def
Jumeil John Navares <jumeil.navares@gmail.com>
2025-07-27 02:25:37 +0800

    Exercise importing external packages - confetti.

---
+++ vite-confetti/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="m-3">
+  <button @click="showConfetti" class="bg-lime-800 text-lime-50 p-2 rounded">Confetti</button>
+</div>
+</template>
+
+<script setup>
+import confetti from 'canvas-confetti';
+
+function showConfetti() {
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+}
+
+</script>
---
+++ vite-vue-frappe-ui/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="m-5">
+    <Badge>Active</Badge>
+  </div>
+</template>
+
+<script setup>
+import Badge from './Badge.vue';
+
+</script>
